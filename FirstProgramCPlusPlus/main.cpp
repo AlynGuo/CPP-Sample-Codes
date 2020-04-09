143e7bd5dfbffa067257f03601facce52ed869f5
Arisa Rin Guo <54733240+AlynGuo@users.noreply.github.com>
2020-04-09 19:40:41 +0800

    Add files via upload

---
+++ FirstProgramCPlusPlus/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+This is my first C++ program!
+*/
+ main()
+{
+    cout << "Hello world!" << endl;
+    cout << "I am learning C++!";
+    return 0;
+}
